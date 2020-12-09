e81504b817cb3889930806745e8e6c2e676e0921
xyq <610108910@qq.com>
2020-12-10 01:26:27 +0800

    首次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diskList = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dirList = new System.Windows.Forms.ListView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // diskList
+            // 
+            this.diskList.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.diskList.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.diskList.FormattingEnabled = true;
+            this.diskList.ItemHeight = 35;
+            this.diskList.Location = new System.Drawing.Point(12, 12);
+            this.diskList.Name = "diskList";
+            this.diskList.Size = new System.Drawing.Size(171, 424);
+            this.diskList.TabIndex = 0;
+            this.diskList.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.diskList_DrawItem);
+            this.diskList.SelectedIndexChanged += new System.EventHandler(this.diskList_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(189, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(599, 424);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnBack);
+            this.tabPage1.Controls.Add(this.dirList);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(591, 398);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dirList
+            // 
+            this.dirList.Location = new System.Drawing.Point(0, 0);
+            this.dirList.Name = "dirList";
+            this.dirList.Size = new System.Drawing.Size(595, 401);
+            this.dirList.TabIndex = 0;
+            this.dirList.UseCompatibleStateImageBehavior = false;
+            this.dirList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dirList_MouseDoubleClick);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(421, 6);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(58, 35);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "button1";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.diskList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox diskList;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView dirList;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string curPath; //当前所在路径
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* 获取所有驱动器，遍历并添加至列表 */
+            DriveInfo[] disks = DriveInfo.GetDrives();
+            foreach (DriveInfo disk in disks)
+            {
+                diskList.Items.Add(disk);
+            }
+            dirList.View = View.List;
+        }
+
+        private void diskList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /* 点击驱动器时，获取对应的目录内容 */
+            curPath = diskList.SelectedItem.ToString();
+            DirectoryInfo dir = new DirectoryInfo(curPath);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            FileInfo[] files = dir.GetFiles();
+
+            dirList.Items.Clear();
+            foreach (DirectoryInfo i in dirs)
+            {
+                if ((i.Name != "$RECYCLE.BIN") && (i.Name != "System Volume Information"))
+                {
+                    dirList.Items.Add(i.Name);
+                }
+            }
+
+            foreach (FileInfo i in files)
+            {
+                dirList.Items.Add(i.Name);
+            }
+        }
+
+        private void diskList_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            /* 驱动器列表自定义重绘 */
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            StringFormat strFmt = new System.Drawing.StringFormat();
+            strFmt.Alignment = StringAlignment.Center;     //文本垂直居中
+            strFmt.LineAlignment = StringAlignment.Center; //文本水平居中
+
+            e.Graphics.DrawString(diskList.Items[e.Index].ToString(), new Font(this.Font.FontFamily, 14), new SolidBrush(e.ForeColor), e.Bounds, strFmt);
+            //e.Graphics.DrawIcon();
+        }
+
+        private void dirList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            curPath = curPath + "\\" + dirList.FocusedItem.Text.ToString();
+            FileInfo f = new FileInfo(curPath);
+            if(f.Exists)
+            {
+                /* 如果是文件，则打开它，并把路径还原 */
+                System.Diagnostics.Process.Start(curPath);
+                curPath = curPath.Substring(0, curPath.LastIndexOf("\\"));
+            }
+            else
+            {
+                /* 如果是文件夹，向下遍历 */
+                DirectoryInfo dir = new DirectoryInfo(curPath);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+
+                dirList.Clear();
+                foreach (DirectoryInfo i in dirs)
+                {
+                    if ((i.Name != "$RECYCLE.BIN") && (i.Name != "System Volume Information"))
+                    {
+                        dirList.Items.Add(i.Name);
+                    }
+                }
+
+                foreach (FileInfo i in files)
+                {
+                    dirList.Items.Add(i.Name);
+                }
+            }
+            
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if ((curPath != null) && (curPath.LastIndexOf("\\") > 0))
+            {
+                curPath = curPath.Substring(0, curPath.LastIndexOf("\\"));
+
+                DirectoryInfo dir = new DirectoryInfo(curPath);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+
+                dirList.Items.Clear();
+                foreach (DirectoryInfo i in dirs)
+                {
+                    if ((i.Name != "$RECYCLE.BIN") && (i.Name != "System Volume Information"))
+                    {
+                        dirList.Items.Add(i.Name);
+                    }
+                }
+
+                foreach (FileInfo i in files)
+                {
+                    dirList.Items.Add(i.Name);
+                }
+            }
+        }
+    }
+}
